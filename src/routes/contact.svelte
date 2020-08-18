3bebfbf85c64973ed86943756fd3b7a114d37dc1
ocupe <jonasschell@ocupe.org>
2020-08-18 19:31:30 +0200

    add: contact page

---
+++ src/routes/contact.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <title>Contact | Jonas Schell</title>
+</svelte:head>
+
+<div class="page">
+  <h1>Get in touch</h1>
+
+  <p>This is the 'about' page. There's not much here.</p>
+</div>
