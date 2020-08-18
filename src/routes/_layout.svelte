3f1a5a8e1b18bcaa2772c68556dee9a6f7ab23a0
ocupe <jonasschell@ocupe.org>
2020-08-18 19:21:19 +0200

    styling updates

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,23 +1,22 @@
 <script>
-    import Nav from "../components/Nav.svelte";
+  import Nav from "../components/Nav.svelte";
 
-    export let segment;
+  export let segment;
 </script>
 
 <Nav {segment} />
 
 <main>
-    <slot />
+  <slot />
 </main>
 
 <style>
-    main {
-        position: relative;
-        width: 100%;
-        max-width: 35em;
-        background-color: white;
-        padding: 0;
-        margin: 0 auto;
-        box-sizing: border-box;
-    }
+  main {
+    position: relative;
+    width: 100%;
+    max-width: 35em;
+    background-color: white;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>
--- src/routes/work/[slug].svelte
+++ src/routes/work/[slug].svelte
@@ -1,67 +1,74 @@
 <script context="module">
-    export async function preload({ params, query }) {
-        // the `slug` parameter is available because
-        // this file is called [slug].svelte
-        const res = await this.fetch(`work/${params.slug}.json`);
-        const data = await res.json();
+  export async function preload({ params, query }) {
+    // the `slug` parameter is available because
+    // this file is called [slug].svelte
+    const res = await this.fetch(`work/${params.slug}.json`);
+    const data = await res.json();
 
-        if (res.status === 200) {
-            return { data: data };
-        } else {
-            this.error(res.status, data.message);
-        }
+    if (res.status === 200) {
+      return { data: data };
+    } else {
+      this.error(res.status, data.message);
     }
+  }
 </script>
 
 <script>
-    export let data;
+  export let data;
 </script>
 
 <div>
-    <img src={data.keyImage.src} alt="{data.keyImage.alt}/" />
-    <article>
-        <h1>{data.title}</h1>
-        <p>{data.text}</p>
-    </article>
+  <img src={data.keyImage.src} alt="{data.keyImage.alt}/" />
+  <article>
+    <h1>{data.title}</h1>
+    <p>{data.text}</p>
+  </article>
 </div>
 
 <style>
-    article {
-        grid-area: text;
-        padding-top: var(--m);
-        padding-left: var(--s);
-        padding-right: var(--s);
-        /* padding: 1.2em; */
-    }
+  article {
+    grid-area: text;
+    padding-top: var(--m);
+    padding-left: var(--s);
+    padding-right: var(--s);
+    /* padding: 1.2em; */
+  }
 
-    /* div {
+  /* div {
         display: flex;
         width: 100%;
         flex-direction: column;
     } */
-    img {
-        /* position: absolute;
-        top: 0px;
-        left: 0px; */
-        grid-area: img;
-        max-width: 100%;
-        /* min-width: 0; */
-    }
-    /* h1 {
+  img {
+    grid-area: img;
+    width: 100vw;
+    height: 30vh;
+    object-fit: cover;
+    padding-left: -var(--xs);
+    /* align-self: center; */
+    place-self: center;
+    /* max-width: 100vw;
+    padding: -10%; */
+
+    /* min-width: 0; */
+  }
+  /* h1 {
         grid-area: title;
     } */
-    /* p {
+  /* p {
         grid-area: text;
     } */
-    div {
-        display: grid;
-        grid-template-columns: 100%;
-        grid-template-rows: 30% auto auto;
-        grid-template-areas:
-            "img"
-            "text"
-            "slider";
-        /* width: 100%;
+  div {
+    display: grid;
+    grid-template-columns: 100%;
+    grid-template-rows: 30% auto auto;
+    grid-template-areas:
+      "img"
+      "text"
+      "slider";
+
+    grid-gap: var(--s);
+    /* width: 100%;
         height: 100px; */
-    }
+  }
 </style>
