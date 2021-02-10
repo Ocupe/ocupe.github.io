03d1c40ef8a2fce4c05cdeaaed5f2f27acd4a7a6
ocupe <jonasschell@ocupe.org>
2021-02-10 11:20:30 +0100

    add redirect

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,6 @@
-<script>
-  import Folder from "../components/Folder.svelte";
-  import { dataset_dev, get_slot_context } from "svelte/internal";
-  import { goto } from "@sapper/app";
-  const redirect = async () => {
-    await goto("/work");
-  };
-  redirect();
+<script context="module">
+  import { redirect } from "@sapper/app";
+  export function preload({ params, query }) {
+    return redirect(302, "work");
+  }
 </script>
-
-<style>
-</style>
